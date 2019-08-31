ab85319d76cbbe3e9aebb4dcf9ded9dbc8c4f61b
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2019-08-31 08:46:06 +0200

    init

---
+++ index.js
@@ -0,0 +1,66 @@
+window.addEventListener('DOMContentLoaded',
+  function () {
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+
+    function draw() {
+      canvas.width = document.body.offsetWidth;
+      canvas.height = document.body.offsetHeight;
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle = '#fafafa';
+
+      var elements = document.querySelectorAll('.berry');
+      var len = elements.length;
+      var lastX = elements[0].offsetLeft + 43;
+
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.strokeStyle = '#7e4d70';
+      ctx.lineWidth = 1;
+
+      ctx.beginPath();
+      ctx.moveTo(lastX, elements[0].offsetTop + 43);
+      for (var i = 1; i < len; i++) {
+        var e = elements[i];
+        var x = e.offsetLeft + 43;
+        var y = e.offsetTop + 43;
+
+        if (x < lastX) {
+          ctx.quadraticCurveTo(x, y - 60, x, y);
+        } else {
+          ctx.quadraticCurveTo(x, y + 60, x, y);
+        }
+
+        lastX = x;
+      }
+      ctx.stroke();
+      ctx.closePath();
+
+      ctx.lineWidth = 2;
+      for (var i = 0; i < len; i++) {
+        var e = elements[i];
+        var x = e.offsetLeft + 43;
+        var y = e.offsetTop + 43;
+
+        ctx.beginPath();
+        ctx.arc(x, y, 43, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.stroke();
+        ctx.fill();
+      }
+    }
+
+    canvas.style['z-index'] = '-1';
+    canvas.style.position = 'absolute';
+    canvas.style.top = '0';
+    canvas.style.left = '0';
+    canvas.style.right = '0';
+
+    document.body.style['background-color'] = 'transparent';
+    document.body.appendChild(canvas);
+
+    window.addEventListener('resize', draw, false);
+    window.addEventListener('animationend', draw, false);
+  },
+  false
+);
