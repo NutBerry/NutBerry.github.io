e358d3180bd7987292ddbc4bf0f7c7d3638514ab
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2020-06-04 05:33:05 +0200

    ditto

--- playground/config.js
+++ playground/config.js
@@ -1,5 +1,5 @@
 window.ROOT_NETWORK = 'ropsten';
 window.ROOT_CHAIN_ID = 3;
-window.BRIDGE_ADDRESS = '0x3db76466f2256d89db1cca550f1f7f91579ad380';
+window.BRIDGE_ADDRESS = '0xa9f96d8761aa05430d3ee2e7dff6b04978f13369';
 window.RPC_URL = `https://${window.BRIDGE_ADDRESS}.fly.dev`;
 //window.RPC_URL = 'http://localhost:8000';
